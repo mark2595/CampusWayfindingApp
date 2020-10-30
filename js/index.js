96de340f3dcbfa325de3f996fdac99965b7063b4
mark2595 <30328652@students.federation.edu.au>
2020-10-30 20:33:14 +1100

    code clean up

--- js/index.js
+++ js/index.js
@@ -51,7 +51,7 @@ var TxtType = function(el, toRotate, period) {
               new TxtType(elements[i], JSON.parse(toRotate), period);
             }
         }
-        // INJECT CSS
+        
         var css = document.createElement("style");
         css.type = "text/css";
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
