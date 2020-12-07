00d5688b77d68153fb01df24e996dbb5dbd79b6f
30129071 <markalbanese@students.federation.edu.au>
2020-12-07 14:23:14 +1100

    Generated maps for Churchill campus at zoom level 17.
    Generated maps for Churchill campus at zoom level 17.

---
+++ Churchill Template/17test3/images/kgo.bat
@@ -0,0 +1,29 @@
+@echo off
+mkdir "118846"
+mkdir "118847"
+mkdir "118848"
+mkdir "118849"
+mkdir "118850"
+
+rem ren 17map_01.png 80657.png
+rem move 80657.png 118846\80657.png
+rem ren 17map_02.png 118847\80657.png
+
+
+move 17map_03.png 118848\80657.png
+move 17map_04.png 118849\80657.png
+move 17map_05.png 118850\80657.png
+
+move 17map_06.png 118846\80658.png
+move 17map_07.png 118847\80658.png
+move 17map_08.png 118848\80658.png
+move 17map_09.png 118849\80658.png
+move 17map_10.png 118850\80658.png
+
+move 17map_11.png 118846\80659.png
+move 17map_12.png 118847\80659.png
+move 17map_13.png 118848\80659.png
+move 17map_14.png 118849\80659.png
+move 17map_15.png 118850\80659.png
+
+pause
