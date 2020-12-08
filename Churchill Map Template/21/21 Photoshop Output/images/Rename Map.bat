6f13bcff7087ae0c5c35f9b715ff72db9b590b6a
30129071 <markalbanese@students.federation.edu.au>
2020-12-08 14:50:02 +1100

    Cleaned up the file structure for map generation
    Cleaned up the file structure for map generation

---
+++ Churchill Map Template/21/21 Photoshop Output/images/Rename Map.bat
@@ -0,0 +1,73 @@
+@echo off
+mkdir "237693"
+mkdir "237694"
+mkdir "237695"
+mkdir "237696"
+mkdir "237697"
+mkdir "237698"
+mkdir "237699"
+mkdir "237700"
+
+move 18_01.png 237693\161314.png
+move 18_02.png 237694\161314.png
+move 18_03.png 237695\161314.png
+move 18_04.png 237696\161314.png
+move 18_05.png 237697\161314.png
+move 18_06.png 237698\161314.png
+move 18_07.png 237699\161314.png
+move 18_08.png 237700\161314.png
+
+move 18_09.png 237693\161315.png
+move 18_10.png 237694\161315.png
+move 18_11.png 237695\161315.png
+move 18_12.png 237696\161315.png
+move 18_13.png 237697\161315.png
+move 18_14.png 237698\161315.png
+move 18_15.png 237699\161315.png
+move 18_16.png 237700\161315.png
+
+move 18_17.png 237693\161316.png
+move 18_18.png 237694\161316.png
+move 18_19.png 237695\161316.png
+move 18_20.png 237696\161316.png
+move 18_21.png 237697\161316.png
+move 18_22.png 237698\161316.png
+move 18_23.png 237699\161316.png
+move 18_24.png 237700\161316.png
+
+move 18_25.png 237693\161317.png
+move 18_26.png 237694\161317.png
+move 18_27.png 237695\161317.png
+move 18_28.png 237696\161317.png
+move 18_29.png 237697\161317.png
+move 18_30.png 237698\161317.png
+move 18_31.png 237699\161317.png
+move 18_32.png 237700\161317.png
+
+move 18_33.png 237693\161318.png
+move 18_34.png 237694\161318.png
+move 18_35.png 237695\161318.png
+move 18_36.png 237696\161318.png
+move 18_37.png 237697\161318.png
+move 18_38.png 237698\161318.png
+move 18_39.png 237699\161318.png
+move 18_40.png 237700\161318.png
+
+
+rem move 17map_03.png 118848\80657.png
+rem move 17map_04.png 118849\80657.png
+rem move 17map_05.png 118850\80657.png
+rem 
+rem move 17map_06.png 118846\80658.png
+rem move 17map_07.png 118847\80658.png
+rem move 17map_08.png 118848\80658.png
+rem move 17map_09.png 118849\80658.png
+rem move 17map_10.png 118850\80658.png
+rem 
+rem move 17map_11.png 118846\80659.png
+rem move 17map_12.png 118847\80659.png
+rem move 17map_13.png 118848\80659.png
+rem move 17map_14.png 118849\80659.png
+rem move 17map_15.png 118850\80659.png
+
+pause
